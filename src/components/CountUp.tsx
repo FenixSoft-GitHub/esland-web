c02fb9be3d62265c49a5da4dd42276d8a7a9611a
Alberto Ortiz <alfaortiz.sis@gmail.com>
2024-08-09 12:53:59 -0400

    Add page Information

--- src/components/CountUp.tsx
+++ src/components/CountUp.tsx
@@ -1,16 +1,15 @@
 import { useProgressiveNumber } from '@/hooks/useProgressiveNumber';
-
-import { useEffect } from 'preact/hooks'
+import { useEffect } from 'preact/hooks';
 
 export const CountUp = (
-    { initial, final, decimal, duration }:
-    { initial: number, final: number, decimal?: number,  duration?: number }
+    { initial, final, decimals, duration }:
+    { initial: number, final: number, decimals?: number,  duration?: number }
 ) => {
-    const [count, setCount] = useProgressiveNumber(initial, duration, decimal)
+    const [count, setCount] = useProgressiveNumber(initial, duration, decimals)
 
     useEffect(() => {
-        setCount(final)
-    }, [])
+    setCount(String(final))
+  }, [final])
     
     return <span>{count}</span>
 }
